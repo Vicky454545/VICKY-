cb3d027a417272f3c84858893478cb211d7e3ecb
RaniShailu <100040797+RaniShailu@users.noreply.github.com>
2024-05-13 08:42:58 +0530

    logger.py

--- logger.py
+++ logger.py
@@ -1,18 +1,18 @@
-import logging   #Bot Created by @NtrRazYt
-from logging.handlers import RotatingFileHandler   #Bot Created by @NtrRazYt
-   #Bot Created by @NtrRazYt
-logging.basicConfig(   #Bot Created by @NtrRazYt
-    level=logging.ERROR,   #Bot Created by @NtrRazYt
-    format=   #Bot Created by @NtrRazYt
-    "%(asctime)s - %(levelname)s - %(message)s [%(filename)s:%(lineno)d]",   #Bot Created by @NtrRazYt
-    datefmt="%d-%b-%y %H:%M:%S",   #Bot Created by @NtrRazYt
-    handlers=[   #Bot Created by @NtrRazYt
-        RotatingFileHandler("Assist.txt", maxBytes=50000000, backupCount=10),   #Bot Created by @NtrRazYt
-        logging.StreamHandler(),   #Bot Created by @NtrRazYt
-    ],   #Bot Created by @NtrRazYt
-)   #Bot Created by @NtrRazYt
-logging.getLogger("pyrogram").setLevel(logging.WARNING)   #Bot Created by @NtrRazYt
-   #Bot Created by @NtrRazYt
-   #Bot Created by @NtrRazYt
-logging = logging.getLogger()   #Bot Created by @NtrRazYt
-   #Bot Created by @NtrRazYt
+import logging   #Bot Created by @𝕃𝕦𝕔𝕚𝕗𝕖𝕣
+from logging.handlers import RotatingFileHandler   #Bot Created by @𝕃𝕦𝕔𝕚𝕗𝕖𝕣
+   #Bot Created by @𝕃𝕦𝕔𝕚𝕗𝕖𝕣
+logging.basicConfig(   #Bot Created by @𝕃𝕦𝕔𝕚𝕗𝕖𝕣
+    level=logging.ERROR,   #Bot Created by @𝕃𝕦𝕔𝕚𝕗𝕖𝕣
+    format=   #Bot Created by @𝕃𝕦𝕔𝕚𝕗𝕖𝕣
+    "%(asctime)s - %(levelname)s - %(message)s [%(filename)s:%(lineno)d]",   #Bot Created by @𝕃𝕦𝕔𝕚𝕗𝕖𝕣
+    datefmt="%d-%b-%y %H:%M:%S",   #Bot Created by @𝕃𝕦𝕔𝕚𝕗𝕖𝕣
+    handlers=[   #Bot Created by @𝕃𝕦𝕔𝕚𝕗𝕖𝕣
+        RotatingFileHandler("Assist.txt", maxBytes=50000000, backupCount=10),   #Bot Created by @𝕃𝕦𝕔𝕚𝕗𝕖𝕣
+        logging.StreamHandler(),   #Bot Created by @𝕃𝕦𝕔𝕚𝕗𝕖𝕣
+    ],   #Bot Created by @𝕃𝕦𝕔𝕚𝕗𝕖𝕣
+)   #Bot Created by @𝕃𝕦𝕔𝕚𝕗𝕖𝕣
+logging.getLogger("pyrogram").setLevel(logging.WARNING)   #Bot Created by @𝕃𝕦𝕔𝕚𝕗𝕖𝕣
+   #Bot Created by @𝕃𝕦𝕔𝕚𝕗𝕖𝕣
+   #Bot Created by @𝕃𝕦𝕔𝕚𝕗𝕖𝕣
+logging = logging.getLogger()   #Bot Created by @𝕃𝕦𝕔𝕚𝕗𝕖𝕣
+   #Bot Created by @𝕃𝕦𝕔𝕚𝕗𝕖𝕣
